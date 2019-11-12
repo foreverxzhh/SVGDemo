aa193f3cbae4de6f0d4dd38c3e4d775715c2adf6
huash0702 <huash0702@thundersoft.com>
2019-11-12 10:20:09 +0800

    Initial commit

---
+++ app/src/main/java/com/demo/svgdemo/MainActivity.java
@@ -0,0 +1,35 @@
+package com.demo.svgdemo;
+
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View view) {
+        ImageView imageView = (ImageView) view;
+        Drawable drawable = imageView.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
+    }
+
+    private boolean isTwitterChecked = false;
+
+    public void twitterClick(View view) {
+        ImageView imageView = (ImageView) view;
+        isTwitterChecked = !isTwitterChecked;
+        final int[] stateSet = {android.R.attr.state_checked * (isTwitterChecked ? 1 : -1)};
+        imageView.setImageState(stateSet, true);
+    }
+
+}
